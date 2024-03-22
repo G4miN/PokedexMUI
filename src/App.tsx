cb380ed06b524d19053eb93fc33cc7c4a374fac8
yeduin <el_yeduin_02@hotmail.com>
2024-03-21 22:17:44 -0400

    Your descriptive commit message

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavbarComponent from './components/Navbar'
+import PokemonComponent from './components/Pokemon';
+
+function App() {
+
+  return (
+    <>
+    <NavbarComponent/>
+    <PokemonComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+export default function NavbarComponent(){
+    return (
+        <>
+            <nav className="navbar">
+                    <ul>
+                    <li><a href="/"><i className="fas fa-home"></i>Home</a></li>          
+                    <li><a href="/about"><i className="fas fa-record-vinyl"></i>Pokedex</a></li>
+                    <li><a href="/contact"><i className="fas fa-gamepad"></i>Videogames</a></li>
+                    <li><a href="/"><i className="fas fa-layer-group"></i>OCC Pokemon</a></li>
+                    <li><a href="/about"><i className="fas fa-tv"></i>TV Pokemon</a></li>
+                    <li><a href="/contact"><i className="fas fa-trophy"></i>Play! Pokemon</a></li>
+                    <li><a href="/contact"><i className="fas fa-newspaper"></i>News</a></li>
+                    </ul>
+            </nav>
+        </>
+    )
+}
