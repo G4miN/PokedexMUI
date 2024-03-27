2f50c9fe68a4aecbbdcd4f3fdf5c55743bf9c322
Yeduin <ymateo@objectlink.net>
2024-03-27 17:15:55 -0400

    adding some styles again fixing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,44 @@
 export default function Navbar({ id }: { id: string }) {
   return (
-    <div>
-      <nav className="navbar" id={id}>
-        <ul>
-          <li>
-            <a href="/">
-              <i className="fas fa-home"></i>Home
-            </a>
-          </li>
-          <li>
-            <a href="/about">
-              <i className="fas fa-record-vinyl"></i>Pokedex
-            </a>
-          </li>
-          <li>
-            <a href="/contact">
-              <i className="fas fa-gamepad"></i>Videogames
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <i className="fas fa-layer-group"></i>OCC Pokemon
-            </a>
-          </li>
-          <li>
-            <a href="/about">
-              <i className="fas fa-tv"></i>TV Pokemon
-            </a>
-          </li>
-          <li>
-            <a href="/contact">
-              <i className="fas fa-trophy"></i>Play! Pokemon
-            </a>
-          </li>
-          <li>
-            <a href="/contact">
-              <i className="fas fa-newspaper"></i>News
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="navbar" id={id}>
+      <ul>
+        <li>
+          <a href="/">
+            <i className="fas icon-pikachu"></i>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/about">
+            <i className="fas icon-pokeball"></i>Pokedex
+          </a>
+        </li>
+        <li>
+          <a href="/contact">
+            <i className="fas icon-joystick"></i>Videogames
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <i className="fas icon-card"></i>OCC Pokemon
+          </a>
+        </li>
+        <li>
+          <a href="/about">
+            <i className="fas icon-animation"></i>TV Pokemon
+          </a>
+        </li>
+        <li>
+          <a href="/contact">
+            <i className="fas icon-trophy"></i>Play! Pokemon
+          </a>
+        </li>
+        <li>
+          <a href="/contact">
+            <i className="fas icon-news"></i>News
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
